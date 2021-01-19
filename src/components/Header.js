fcf532cdb6ebd43bb12ac1c32efb5a0499dfa18b
Mo <mcresp495@gmail.com>
2021-01-19 19:47:51 +0200

    make a responsive design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Header.css'
 import TeslaLogo from '../assets/teslaLogoSmall.svg'
+import MenuIcon from '@material-ui/icons/Menu';
 function Header() {
     return (
         <div className="header">
                 <div className="header__logo">
                     <img src={TeslaLogo} alt="Tesla Logo" />
                 </div>
-                <div className="header__center" >
+                <div className="header__center" id="header__center" >
                     <p>Model 5</p>
                     <p>Model 3</p>
                     <p>Model X</p>
@@ -18,6 +19,7 @@ function Header() {
                 <div className="header__right">
                     <p>Shop</p>
                     <p>Tesla Account</p>
+                    <MenuIcon className="menuIcon" />
                 </div>
         </div>
     )
